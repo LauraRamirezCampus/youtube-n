c3760ba4e07973d64746c325a04d531e3dd7951b
LauraRamirezCampus <lauravramirez0327@gmailcom>
2023-05-22 15:45:53 -0500

    feat: :sparkles: muestra el video y los comentarios relacionados
    se modifica la funcion comentarios para que muestre los comentarios que estan relacionados con el video que se busco

--- main.js
+++ main.js
@@ -5,8 +5,11 @@
 //        console.log(data);
 //})}
 //yout()}
-async function comentarios(){
-    const url = 'https://youtube138.p.rapidapi.com/video/comments/?id=urZ0shWe7is&hl=en&gl=US';
+
+let id="";
+async function video(){
+
+	const url = `https://youtube138.p.rapidapi.com/search/?q=${busca.value}&hl=en&gl=US`;
 const options = {
 	method: 'GET',
 	headers: {
@@ -18,9 +21,41 @@ const options = {
 try {
 	const response = await fetch(url, options);
 	const result = await response.json();
+	console.log(result);
+	console.log(result.contents[0].video.videoId);
+	let idd=result.contents[0].video.videoId;
+	console.log(idd);
+	id=idd;
+	document.getElementById("video").innerHTML=`<iframe width="560" height="315" src="https://www.youtube.com/watch?v=ixZ_3A5jzk0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+	comentarios()
+	console.log(id,"id");
+} catch (error) {
+	console.error(error);
+}
+
+
+}
+
+
+
+async function comentarios(){
+	document.getElementById("comentarios").innerHTML="";
+	const urlc = `https://youtube138.p.rapidapi.com/video/comments/?id=${id}&hl=en&gl=US`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2791634466msh68bd6379ab5dfdap1cb8bbjsnc5d9de46ed9e',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
+	try {
+		console.log(id,"sip");
+		const response = await fetch(urlc, options);
+		console.log(urlc),"l";
+	const result = await response.json();
 	console.log(result,);
-	console.log(result.comments[0].author,"A");
-	console.log(result.comments[0].author.avatar[0].url);
+	//console.log(result.comments[1].author,"A");
+	//console.log(result.comments[0].author.avatar[0].url);
     //console.log(result.comments[0].content ,"A"); 
     for(let i=0; i< result.comments.length; i++){
     document.getElementById("comentarios").innerHTML+=`<div class="">
@@ -40,40 +75,20 @@ try {
 	}
 }
 
-async function video(){
-
-	const url = `https://youtube138.p.rapidapi.com/search/?q=${busca.value}&hl=en&gl=US`;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '2791634466msh68bd6379ab5dfdap1cb8bbjsnc5d9de46ed9e',
-		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-}
-
-
 
 
 
 
 
 let busca= document.getElementById("buscador");
+let boton= document.getElementById("btnGroupAddon2");
 
-busca.addEventListener("input",(e)=>{
-	video()
-	console.log(busca.value);
-})
+ boton.addEventListener("click",(e)=>{
+ 	video()
+ 	comentarios()
+ 	console.log(busca.value);
+ })
 
 
-comentarios()
 
 
